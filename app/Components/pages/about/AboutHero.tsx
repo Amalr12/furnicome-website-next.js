d2c206d412a24cc0a762b0357c2f9d2f7ec63386
Amalr12 git config --global user.name Amalr12 <amaldaspr12@gmail.com>
2026-03-02 11:16:26 +0530

    about section

--- app/Components/pages/about/AboutHero.tsx
+++ app/Components/pages/about/AboutHero.tsx
@@ -1,6 +1,7 @@
 export default function AboutHero() {
     return(
-        <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+        <>
+             <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
             <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
                 <h1 className="text-5xl font-bold text-gray-900 dark:text-white sm:text-6xl">
                     About Us
@@ -10,5 +11,7 @@ export default function AboutHero() {
                 </p>
             </main>
         </div>
+        </>
+       
     );
 }
--- app/about/Page.tsx
+++ app/about/Page.tsx
@@ -1,6 +1,12 @@
+import AboutHero from "../Components/pages/about/AboutHero";
+
 export default function About(){
     return(
         <>
+        <main>
+        <AboutHero />
+        </main>
+        
         </>
     );
 }
