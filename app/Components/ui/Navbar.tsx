e2e166332fae9f88c143ade5b50d5517298bb839
Amalr12 git config --global user.name Amalr12 <amaldaspr12@gmail.com>
2026-03-02 09:52:39 +0530

    updated

--- app/Components/ui/Navbar.tsx
+++ app/Components/ui/Navbar.tsx
@@ -1,11 +1,13 @@
+import Link from "next/link";
+
 export default function Navbar() {
     return(
         <nav className="flex items-center justify-between bg-gray-800 p-6 text-white">
             <div className=" font-bold text-2xl hover:text-gray-400">FURNICOME</div>
             <ul className="flex space-x-4 text-xl">
-                <li><a href="/" className="hover:text-gray-400 font-bold">Home</a></li>
-                <li><a href="/about" className="hover:text-gray-400 font-bold">About</a></li>
-                <li><a href="/contact" className="hover:text-gray-400 font-bold">Contact</a></li>
+              <Link href="/">  <li><p className="hover:text-gray-400 font-bold">Home</p></li></Link>
+               <Link href="/about"> <li><p className="hover:text-gray-400 font-bold">About</p></li></Link>
+               <Link href="/contact"> <li><p className="hover:text-gray-400 font-bold">Contact</p></li></Link>
             </ul>
         </nav>
     );
